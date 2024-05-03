0c8cecef3142c920c1a616691ccaa251acbbffb6
ParthP3 <parthneilpujari@gmail.com>
2024-05-03 17:48:00 +0530

    Added debluring

--- Project/poisson.py
+++ Project/poisson.py
@@ -9,14 +9,14 @@ def add_poisson_noise(x,lam):
 def blur(x):
     return cv2.GaussianBlur(x, (9,9), sigmaX=100, sigmaY=100)
 
-def denoise(x,lam):
+def denoise(x,y,s,lam):
     k = 0
     u = (np.zeros_like(x, dtype=np.float64))
     v = (np.zeros_like(x, dtype=np.float64))
     while True:
         x1 = x
         x = ((lam*(v-u)-1)+np.sqrt((lam*(v-u)-1)**2+4*lam*y))/(2*lam)
-        z = np.reshape(x+u, [image1.shape[0], image1.shape[1]])
+        z = np.reshape(x+u, [s,s])
         v = bm3d(z, np.sqrt(1/lam))
         v = np.reshape(v, -1)
         u = u+(x-v)
@@ -29,7 +29,7 @@ def denoise(x,lam):
         k = k+1
     return x
 
-def deblur(x, s, lam):
+def deblur(x, y, s, lam):
     one = np.ones_like(x)
     k = 0
     u = (np.zeros_like(x, dtype=np.float64))
@@ -39,12 +39,12 @@ def deblur(x, s, lam):
         k1 = 0
         while True:
             x2 = x
-            temp = cv2.GaussianBlur(np.reshape(x,[s,s]), (9, 9), sigmaX=10, sigmaY=10)
+            temp = cv2.GaussianBlur(np.array(np.reshape(x,[s,s]), np.uint8), (9, 9), sigmaX=10, sigmaY=10)
             temp = np.reshape(temp, -1)
-            temp = y/temp
-            temp = cv2.GaussianBlur(np.reshape(temp,[s,s]), (9, 9), sigmaX=10, sigmaY=10)
+            temp = y/np.array(temp, np.float64)
+            temp = cv2.GaussianBlur(np.array(np.reshape(temp,[s,s]),np.uint8), (9, 9), sigmaX=10, sigmaY=10)
             temp = np.reshape(temp, -1)
-            temp1 = cv2.GaussianBlur(np.reshape(one,[s,s]), (9, 9), sigmaX=10, sigmaY=10)
+            temp1 = cv2.GaussianBlur(np.array(np.reshape(one,[s,s]),np.uint8), (9, 9), sigmaX=10, sigmaY=10)
             temp1 = np.reshape(temp1, -1)
             x = x- ((-temp)+temp1 + lam*(x-v+u))
             if np.linalg.norm(x2-x)<1:
@@ -61,40 +61,45 @@ def deblur(x, s, lam):
             print(np.linalg.norm(x1-x))
         if np.linalg.norm(x1-x) < 0.1:
             break
-        if k > 50:
+        if k > 40:
             break
         k = k+1
     return x
 #LOAD IMAGE
 image = Image.open("barbara.jpg")
 image1 = np.asarray(image)
-image1 = image1[0:256, 0:256]
 plt.imshow(image1, cmap="gray")
 plt.show()
 plt.savefig("barb_crop.png")
 X = np.reshape(image1,-1)
-print(X.shape)
 
 lam = 100
 y = blur(np.reshape(X, [image1.shape[0], image1.shape[1]]))
-y = np.reshape(y, -1)
-y = add_poisson_noise(y,lam)
+y1 = np.reshape(y, -1)
+y2 = add_poisson_noise(y1,lam)
+y3 = add_poisson_noise(X, lam*10)
 
-y1 = np.reshape(y, [image1.shape[0], image1.shape[1]])
+y_noise = np.reshape(y3, [image1.shape[0], image1.shape[1]])
+y_blur = np.reshape(y2, [image1.shape[0], image1.shape[1]])
+
 #SHOW NOISY IMAGE
-plt.imshow(y1, cmap="gray")
+plt.imshow(y_noise, cmap="gray")
 plt.show()
-plt.savefig("noise_barb.png")
+plt.savefig("./denoise/noise_barb.png")
 
+#SHOW BLURRY IMAGE
+plt.imshow(y_blur, cmap="gray")
+plt.show()
+plt.savefig("./deblur/blurry_barb.png")
+x_denoise = denoise(y3,y3,image1.shape[0],lam*10)
+x_deblur = deblur(y2, y2, image1.shape[0],lam)
 
-#x_denoise = denoise(y,lam)
-x_deblur = deblur(y,image1.shape[0],lam)
-#recon_denoise = np.reshape(x_denoise, [image1.shape[0], image1.shape[1]])
+recon_denoise = np.reshape(x_denoise, [image1.shape[0], image1.shape[1]])
 recon_deblur = np.reshape(x_deblur, [image1.shape[0], image1.shape[1]])
 
-#plt.imshow(recon_denoise, cmap="gray")
-#plt.show()
-#plt.savefig("recon_denoise.png")
+plt.imshow(recon_denoise, cmap="gray")
+plt.show()
+plt.savefig("./denoise/recon_barbara_denoise.png")
 plt.imshow(recon_deblur, cmap="gray")
 plt.show()
-plt.savefig("recon_denoise.png")
+plt.savefig("./deblur/recon_barbara_deblur.png")
